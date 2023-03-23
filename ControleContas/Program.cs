52ed17b2ee921f0fc85398154f3a9febd627a840
202010439 <202010439@TPCP04LAB1012>
2023-03-22 21:07:40 -0300

    relacionamento entre cliente e conta

--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -1,12 +1,16 @@
 ﻿
 using ControleContas.Model;
 
-Conta conta = new Conta("1234");
-Conta conta2 = new Conta("4321", 1000.99m);
-Conta conta3 = new Conta("54321", 4000.99m);
+Cliente cliente1 = new Cliente("Fred", "13282739273", 2003);
+Conta conta = new Conta("1234", cliente1);
+Conta conta2 = new Conta("4321", 1000.99m, cliente1);
+Conta conta3 = new Conta("54321", 4000.99m, cliente1);
 
 Console.WriteLine($"Conta: {conta.Numero}; Saldo: {conta.Saldo}");
 Console.WriteLine($"O saldo total é {conta2.saldoTotal}");
 Console.WriteLine($"A conta de maior saldo é {conta2.ContaMaiorSaldo}");
 
-Cliente cliente1 = new Cliente("Fred", "13282739273", 2003);
+conta.Titular = cliente1;
+conta2.Titular = cliente1;
+
+Console.WriteLine($"O titular da conta {conta.Numero} é o cliente {conta.Titular.Nome}");
